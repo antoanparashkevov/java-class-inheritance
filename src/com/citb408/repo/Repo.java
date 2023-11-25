d7433bdc8e20e5cadd66117a41c320d9cb5ea2da
“antoanparashkevov” <tonkata1505@gmail.com>
2023-11-25 12:29:48 +0200

    Create Repo class

---
+++ src/com/citb408/repo/Repo.java
@@ -0,0 +1,49 @@
+package com.citb408.repo;
+
+public class Repo {
+    private double maxSize;
+    private int currentNumberOfDocuments;
+    private double currentSize;
+
+    public Repo(double maxSize, int currentNumberOfDocuments, double currentSize) {
+        this.maxSize = maxSize;
+        this.currentNumberOfDocuments = currentNumberOfDocuments;
+        this.currentSize = currentSize;
+    }
+
+    public double getMaxSize() {
+        return maxSize;
+    }
+
+    public int getCurrentNumberOfDocuments() {
+        return currentNumberOfDocuments;
+    }
+
+    public double getCurrentSize() {
+        return currentSize;
+    }
+
+    public double freeSpace() {
+        return this.maxSize - this.currentSize;
+    }
+
+    public boolean uploadDocument(Document document) {
+        if(this.freeSpace() >= document.getSize()) {
+            this.currentNumberOfDocuments++;
+            this.currentSize+=document.getSize();
+            System.out.println(document.getName() + " is uploaded!");
+            return true;
+        }
+        System.out.println(document.getName() + " is not uploaded! Not enough space!");
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Repo{" +
+                "maxSize=" + maxSize +
+                ", currentNumberOfDocuments=" + currentNumberOfDocuments +
+                ", currentSize=" + currentSize +
+                '}';
+    }
+}
